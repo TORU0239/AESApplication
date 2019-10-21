dbf2aca2a2ccca692d794b909c1cb113dd6b5e0c
Toru Choi <blueocean239@gmail.com>
2019-10-21 16:33:53 +0800

    implement using MVVM pattern and AES

--- app/src/main/java/sg/toru/aesapplication/MainViewModel.kt
+++ app/src/main/java/sg/toru/aesapplication/MainViewModel.kt
@@ -3,14 +3,24 @@ package sg.toru.aesapplication
 import android.app.Application
 import android.util.Log
 import android.view.View
+import androidx.databinding.ObservableField
 import androidx.lifecycle.AndroidViewModel
 
 class MainViewModel(app:Application):AndroidViewModel(app) {
     private val TAG = MainViewModel::class.java.simpleName
 
+    val userIdLiveData = ObservableField<String>("")
+    val userPwdLiveData = ObservableField<String>("")
+    val encryptedData = ObservableField<String>("")
+
     fun onSignInButtonClick(view: View){
-        Log.e(TAG, "onSignInClick")
+        userPwdLiveData.get()?.let {
+            if(it.isNotEmpty()){
+                val key = generateKey()
+                Log.e(TAG, "encrypted: ${encrypt(it, key)}")
+                val enc = encrypt(it, key)
+                encryptedData.set(String(enc).plus(", decoded::").plus(decrypt(enc, key)))
+            }
+        }
     }
-
-
 }
